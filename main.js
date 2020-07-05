b996753f95e043a8c789a570d8304006e75f8427
Alexander Brahar <bragardlin@gmail.com>
2020-07-05 16:23:19 +0300

    15 practic 2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+"use strict"
 const numberOfFilms = +prompt('Скільки фільмів ви продивились?', '');
 const personalMovieDB = {
     count: numberOfFilms,
@@ -7,12 +8,23 @@ const personalMovieDB = {
     privat: false,
 };
 
-let movie = prompt('Один із останніх переглянутих фільмів?', ''),
-    evaluation = prompt('На скільки іи його оціните','');
-personalMovieDB.movies[movie] = evaluation;
+for (let i = 0; i < 2; i++){
+    const movie = prompt('Один із останніх переглянутих фільмів?', ''),
+          evaluation = prompt('На скільки іи його оціните', '');
+    if ((movie != null && movie != '' && movie.length <= 50) && (evaluation != null && evaluation != '')){
+        personalMovieDB.movies[movie] = evaluation;
+    } else {
+        i--;
+    }
+}
 
-movie = prompt('Один із останніх переглянутих фільмів?', '');
-evaluation = prompt('На скільки іи його оціните', '');
-personalMovieDB.movies[movie] = evaluation;
-
+if (personalMovieDB.count < 10) {
+    console.log('Переглянуто моло фільмів');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Ви класичний глядач');
+} else if (personalMovieDB.count > 30) {
+    console.log('Ви кіноман');
+} else {
+    console.log('Виникла помилка');
+}
 console.log(personalMovieDB);
