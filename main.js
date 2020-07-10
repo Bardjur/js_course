7c0d8608f768fb283a8317ac6081abe6549445c0
Alexander Brahar <bragardlin@gmail.com>
2020-07-10 21:32:55 +0300

    practic 3

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-"use strict"
-const numberOfFilms = +prompt('Скільки фільмів ви продивились?', '');
+"use strict";
+
+let numberOfFilms;
+
+function start() {
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('Скільки фільмів ви продивились?', '');
+    }
+
+}
+
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,23 +19,45 @@ const personalMovieDB = {
     privat: false,
 };
 
-for (let i = 0; i < 2; i++){
-    const movie = prompt('Один із останніх переглянутих фільмів?', ''),
-          evaluation = prompt('На скільки іи його оціните', '');
-    if ((movie != null && movie != '' && movie.length <= 50) && (evaluation != null && evaluation != '')){
-        personalMovieDB.movies[movie] = evaluation;
-    } else {
-        i--;
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++){
+        const movie = prompt('Один із останніх переглянутих фільмів?', ''),
+              evaluation = prompt('На скільки іи його оціните', '');
+        if ((movie != null && movie != '' && movie.length <= 50) && (evaluation != null && evaluation != '')){
+            personalMovieDB.movies[movie] = evaluation;
+        } else {
+            i--;
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('Переглянуто моло фільмів');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    console.log('Ви класичний глядач');
-} else if (personalMovieDB.count > 30) {
-    console.log('Ви кіноман');
-} else {
-    console.log('Виникла помилка');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Переглянуто моло фільмів');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log('Ви класичний глядач');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Ви кіноман');
+    } else {
+        console.log('Виникла помилка');
+    }
 }
-console.log(personalMovieDB);
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGeners(){
+    for (let i = 1; i <= 3; i++){
+        personalMovieDB.genres[i - 1] = prompt(`Ваш улюблений жанр під номером ${i}`,'');
+    }
+}
+
